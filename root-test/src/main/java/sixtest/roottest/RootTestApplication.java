280501d9b42e9203d0f5d87a46dfcd6ff6d212ad
unknown <2799018587@qq.com>
2019-12-01 21:40:35 +0800

    测试3

---
+++ root-test/src/main/java/sixtest/roottest/RootTestApplication.java
@@ -0,0 +1,13 @@
+package sixtest.roottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootTestApplication.class, args);
+    }
+
+}
